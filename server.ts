9e945f3f5885f16334246615f74843cb576b7b9c
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2019-11-02 03:16:18 -0500

    DropZone and basic GoogleAuth

--- server.ts
+++ server.ts
@@ -1,4 +1,7 @@
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
 const http = require('http');
 const request = require('request');
 const fs = require('fs');
@@ -7,11 +10,15 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
+const { OAuth2Client } = require('google-auth-library');
+
 const DEV_MODE = (process.argv.length > 2);
 const PORT = 3000;
+const CLIENT_ID = "883452357556-rsf99lsl7dl28f092b86q5j5aqk989bf.apps.googleusercontent.com";
 
 const app = express();
 const server = http.createServer(app);
+const client = new OAuth2Client(CLIENT_ID);
 
 if (DEV_MODE) {
     const config = require('./webpack.dev.js');
@@ -27,6 +34,30 @@ if (DEV_MODE) {
     })
     app.use(express.static(__dirname + '/dist'));
 }
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID
+    });
+    const payload = ticket.getPayload();
+    const userId = payload['sub'];
+    return userId;
+}
+
+app.post('/googleAuth', async (req, res) => {
+    try {
+        console.log(req.body);
+        const userId = await verify(req.body.tokenId);
+        res.send({userId: userId});
+    } catch (err) {
+        console.error(err.stack);
+        res.status(500).send({error: err.stack});
+    }
+});
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { StyledDropZone } from 'react-drop-zone';
+import GoogleLogin from 'react-google-login';
+import request from 'browser-request';
+import url from 'url';
+import 'react-drop-zone/dist/styles.css';
 require('./App.css');
+
+function handleDrop(file, text) {
+    console.log(file);
+}
+
+function handleSuccess(response) {
+    const tokenId = response.Zi.id_token;
+    console.log(tokenId);
+    request.post({
+        uri: url.resolve(location.href, '/googleAuth'),
+        json: {
+            tokenId: tokenId
+        }
+    }, (err, res, body) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(body);
+        }
+    });
+}
+
+function handleFailure(response) {
+    console.log(response.error);
+}
 
 function App(props) {
     return (
-        <h1 id="title">Initial test!</h1>
+        <>
+            <h1 id="title">Initial test!</h1>
+            <StyledDropZone onDrop={handleDrop}></StyledDropZone>
+            <GoogleLogin 
+                clientId = "883452357556-rsf99lsl7dl28f092b86q5j5aqk989bf.apps.googleusercontent.com"
+                buttonText = "Login"
+                onSuccess = {handleSuccess}
+                onFailure = {handleFailure}
+                cookiePolicy = {'single_host_origin'}
+            />
+        </>
     );
 }
 
