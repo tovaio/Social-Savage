1347c5b193287fbeb65633b4be26f74a0daec754
Yash Shekar <yshekar3@gatech.edu>
2019-11-02 21:23:41 -0400

    Added an infinite scroll component to the RateView

--- src/RateView.jsx
+++ src/RateView.jsx
@@ -1,10 +1,43 @@
-class RateView extends React.Component {
+import InfiniteScroll from 'react-infinite-scroll-component';
 
+const style = {
+    height: 30,
+    border: "1px solid green",
+    margin: 6,
+    padding: 8
+};
+
+class RateView extends React.Component {
+    state = {
+        items: Array.from({ length: 20 })
+    };
+    fetchMoreData = () => {
+        // a fake async api call like which sends
+        // 20 more records in 1.5 secs
+        setTimeout(() => {
+            this.setState({
+                items: this.state.items.concat(Array.from({ length: 20 }))
+            });
+        }, 1500);
+    };
     render() {
         return (
-            <p>
-                rate
-            </p>
+            <div>
+                <h1>Help out other users by rating their posts!</h1>
+                <hr />
+                <InfiniteScroll
+                    dataLength={this.state.items.length}
+                    next={this.fetchMoreData}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                >
+                    {this.state.items.map((i, index) => (
+                        <div style={style} key={index}>
+                            div - #{index}
+                        </div>
+                    ))}
+                </InfiniteScroll>
+            </div>
         );
     }
 
