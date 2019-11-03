ca50a56184d638b424d4c92e92c28a35063799f8
Laphets <wenqing4@illinois.edu>
2019-11-03 01:43:32 -0600

    Add ui for index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 import LoginView from './LoginView';
 import NavbarLogin from './NavbarLogin';
@@ -34,26 +35,45 @@ class App extends React.Component {
     }
 
     render() {
+
         return (
-            <Container fluid={true}>
-                <Navbar>
-                    <Navbar.Brand>ImageCritique</Navbar.Brand>
-                    <Navbar.Toggle />
-                    <NavbarTabs loginInfo = {this.state.loginInfo} onUpdateCurrentView = {this.handleUpdateCurrentView} />
-                    <NavbarLogin loginInfo = {this.state.loginInfo} onUpdateLoginInfo = {this.handleUpdateLoginInfo} />
-                </Navbar>
-                {
-                    !(this.state.loginInfo.loggedIn) ?
-                        <LoginView onUpdateLoginInfo = {this.handleUpdateLoginInfo} />
-                    : (this.state.currentView == 0) ?
-                        <UploadView loginInfo = {this.state.loginInfo}/>
-                    : (this.state.currentView == 1) ?
-                        <FeedbackView loginInfo = {this.state.loginInfo}/>
-                    : (this.state.currentView == 2) ?
-                        <RateView loginInfo = {this.state.loginInfo}/>
-                    : null
-                }
-            </Container>
+            
+            <div style={{
+                height: '100vh',
+                padding: '30px',
+                background: '#ffffff',
+                backgroundImage: `url(https://demos.creative-tim.com/argon-design-system-pro-angular/assets/img/ill/404.svg)`,
+                backgroundPosition: 'center', /* Center the image */
+  backgroundRepeat: 'no-repeat', /* Do not repeat the image */
+  backgroundSize: 'cover' /* Resize the background image to cover the entire container */
+                
+            }}>
+                <Container fluid={true}>
+
+                    {
+                        this.state.loginInfo.loggedIn && 
+                        <Navbar bg="light" variant="light">
+                            <Navbar.Brand>ImageCritique</Navbar.Brand>
+                            <Navbar.Toggle />
+                            <NavbarTabs loginInfo={this.state.loginInfo} onUpdateCurrentView={this.handleUpdateCurrentView} />
+                            <NavbarLogin loginInfo={this.state.loginInfo} onUpdateLoginInfo={this.handleUpdateLoginInfo} />
+                        </Navbar>
+                    }
+                    
+                    {
+                        !(this.state.loginInfo.loggedIn) ?
+                            <LoginView onUpdateLoginInfo={this.handleUpdateLoginInfo} />
+                            : (this.state.currentView == 0) ?
+                                <UploadView loginInfo={this.state.loginInfo} />
+                                : (this.state.currentView == 1) ?
+                                    <FeedbackView loginInfo={this.state.loginInfo} />
+                                    : (this.state.currentView == 2) ?
+                                        <RateView loginInfo={this.state.loginInfo} />
+                                        : null
+                    }
+                </Container>
+            </div>
+
         );
     }
 
